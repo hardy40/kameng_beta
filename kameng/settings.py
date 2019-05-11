7bb31afa96b54f88e826939d0db8d4a25bcf9319
hardy <hardikatyal@gmail.com>
2019-05-11 16:30:17 +0530

    For deployment

--- kameng/settings.py
+++ kameng/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
     "10.9.1.21",
     "localhost",
     "127.0.0.1",
+    "KamengHostel.pythonanywhere.com"
 ]
 
 
@@ -129,6 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/home/'
